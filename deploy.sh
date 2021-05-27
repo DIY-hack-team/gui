47d5d8889d6ba8d570d96376dd149777a430dd08
Stepan Perlov <stepan.perlov@leroamerlin.ru>
2021-05-28 01:32:13 +0300

    Remove nginx configuration logic

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,7 @@ npm run build
 
 rm -rf build
 mkdir build
-sudo tar cfz build/gui.tar.gz etc dist
-sudo chown $(id -un):$(id -gn) build/gui.tar.gz
+tar cfz build/gui.tar.gz dist
 ssh ${DEPLOY_USER}@${DEPLOY_HOST} "rm -rf /opt/gui; mkdir /opt/gui"
 scp -r ${curDir}/build/gui.tar.gz ${DEPLOY_USER}@${DEPLOY_HOST}:/opt/gui/gui.tar.gz
-ssh ${DEPLOY_USER}@${DEPLOY_HOST} "cd /opt/gui && tar xf gui.tar.gz && sudo chown root:root /opt/gui/etc.nginx-selfsigned.key && sudo chmod 600 /opt/gui/etc.nginx-selfsigned.key && sudo service nginx restart"
+ssh ${DEPLOY_USER}@${DEPLOY_HOST} "cd /opt/gui && tar xf gui.tar.gz"
